610419386dd486c06039ef26467dd299c02e2136
SauceCode01 <sauce.code.01@gmail.com>
2025-12-12 15:19:50 +0800

    feat: target always visible

--- src/components/SparkRush/components/PreviewPane.tsx
+++ src/components/SparkRush/components/PreviewPane.tsx
@@ -28,11 +28,11 @@ export function PreviewPane({
       className={`flex flex-col rounded-xl border-2 h-full transition-colors duration-300 ease-in-out font-sans ${
         displayTarget
           ? "bg-yellow-100 border-yellow-400"
-          : "bg-white border-gray-200"
+          : "bg-yellow-100 border-yellow-400"
       }`}
     >
       <h3 className="text-2xl font-extrabold text-gray-800 capitalize tracking-wide p-6 pb-4 border-b border-gray-200">
-        {displayTarget ? "Target Design" : "Your Output"}
+        {displayTarget ? "Target Design" : "Target Design"}
       </h3>
 
       <div
@@ -40,12 +40,21 @@ export function PreviewPane({
         onMouseLeave={() => setShowTarget(false)}
         className="flex-1 relative rounded-b-xl overflow-hidden bg-white"
       >
-        <iframe
+        {/* <iframe
           key="user"
           title="User Output"
           srcDoc={userHtml}
           className={`absolute inset-0 w-full h-full transition-opacity border-none ${
             displayTarget ? "opacity-0 pointer-events-none" : "opacity-100"
+          }`}
+        /> */}
+
+<iframe
+          key="targetsssssdsfsdfsafdas"
+          title="Target Output"
+          srcDoc={targetHtml}
+          className={`absolute inset-0 w-full h-full transition-opacity border-none ${
+             displayTarget ? "opacity-0 pointer-events-none" : "opacity-100"
           }`}
         />
 
